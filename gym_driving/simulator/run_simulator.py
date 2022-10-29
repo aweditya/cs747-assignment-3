d66bc8b0f85d3b4feb6a74bb1b676fb3dda4d3f0
Aditya Sriram <aweditya@gmail.com>
2022-10-29 18:53:26 +0530

    Added high level features, logic for REINFORCE. Still not working

--- gym_driving/simulator/run_simulator.py
+++ gym_driving/simulator/run_simulator.py
@@ -1,3 +1,4 @@
+from secrets import choice
 from gym_driving.assets.car import *
 from gym_driving.envs.environment import *
 from gym_driving.envs.driving_env import *
@@ -50,32 +51,39 @@ class Task1():
         vel = state[2]
         angle = (state[3] + 360) % 360
 
-        x_bin = np.linspace(-400, 400, 800 // 25 + 1)
-        y_bin = np.linspace(-400, 400, 800 // 25 + 1)
-        vel_bin = np.linspace(0, 20, 20 * 2 + 1)
-        angle_bin = np.linspace(0, 360, 360 // 3 + 1)
+        x_bin = np.linspace(-400, 400, 10 + 1)
+        y_bin = np.linspace(-400, 400, 10 + 1)
+        vel_bin = np.linspace(0, 20, 20 // 5 + 1)
+        angle_bin = np.linspace(0, 360, 10 + 1)
 
-        feature_x = np.zeros(800 // 25)
+        feature_x = np.zeros(10)
         feature_x[np.where(x_bin <= x)[0][-1]] = 1
         
-        feature_y = np.zeros(800 // 25)
+        feature_y = np.zeros(10)
         feature_y[np.where(y_bin <= y)[0][-1]] = 1
 
-        feature_vel = np.zeros(20 * 2)
+        feature_vel = np.zeros(20 // 5)
         feature_vel[np.where(vel_bin <= vel)[0][-1]] = 1
 
-        feature_angle = np.zeros(360 // 3)
+        feature_angle = np.zeros(10)
         feature_angle[np.where(angle_bin <= angle)[0][-1]] = 1
 
         feature = np.concatenate([feature_x, feature_y, feature_vel, feature_angle])
+        # feature = np.concatenate([feature_x, feature_y, feature_angle])
         return feature
-    
-    # def get_feature(self, state):
-    #     state[0] /= 350
-    #     state[1] /= 350
-    #     state[2] /= 20
-    #     state[3] /= 360
-    #     return state
+
+    def get_high_level_features(self, state):
+        x = state[0]
+        y = state[1]
+        vel = state[2]
+        angle = state[3]
+
+        dist_goal = math.sqrt((350 - x) ** 2 + y ** 2)
+        angle_goal_bottom = math.atan((y - 75) / (350 - x)) * 180 / (2 * np.pi)
+        angle_goal_top = math.atan((y + 75) / (350 - x)) * 180 / (2 * np.pi)
+
+        # print(dist_goal, angle_goal_bottom, angle_goal_top)
+        return [dist_goal / (350 * math.sqrt(5)), angle_goal_bottom / 90, angle_goal_top / 90, angle / 180]
 
     def __init__(self):
         """
@@ -83,24 +91,81 @@ class Task1():
         """
         super().__init__()
 
-        # self.d = 4 
-        self.d = (800 // 25) + (800 // 25) + (20 * 2) + (360 // 3)
-        self.w_steer = np.zeros((3, self.d))
-        self.w_acc = np.zeros((5, self.d))
+        self.d = 4 
+        # self.d = 10 + 10 + 4 + 10
+        self.w_steer = np.random.normal(size=(3, self.d))
+        self.w_acc = np.random.normal(size=(5, self.d))
 
         # Eligibility traces
         self.z_steer = np.zeros(self.d)
         self.z_acc = np.zeros(self.d)
 
-    def linear_sarsa_lambda(self, s, a, s_dash, a_dash, r, alpha=0.125, gamma = 1.0, Lambda = 0.9):
-        delta_steer = r + gamma * self.w_steer[a_dash[0]] @ s_dash - self.w_steer[a[0]] @ s
+        # Parametric policy
+        self.pi_steer = np.ones(3) / 3
+        self.pi_acc = np.ones(5) / 5
+
+
+    def reinforce(self, history, alpha=0.1):
+        w_steer_new = np.copy(self.w_steer)
+        w_acc_new = np.copy(self.w_acc)
+
+        s = history['s']
+        a = history['a']
+        r = history['r']
+
+        T = len(s)
+        G = 0.0
+        for k in range(T):
+            G += r[k]
+        
+        grad_ln_pi_steer = np.zeros(np.shape(self.w_steer))
+        grad_ln_pi_acc = np.zeros(np.shape(self.w_acc))
+        for t in range(T):
+            choice_steer = self.w_steer @ self.get_feature(s[t])
+            choice_steer -= np.max(choice_steer)
+            self.pi_steer = np.exp(choice_steer) / np.sum(np.exp(choice_steer))
+            for action_steer in range(3):
+                if action_steer == a[t][0]:
+                    grad_ln_pi_steer[action_steer] = (1 - self.pi_steer[action_steer]) * self.get_feature(s[t])
+                else:
+                    grad_ln_pi_steer[action_steer] = -self.pi_steer[action_steer] * self.get_feature(s[t])
+
+            w_steer_new += alpha * G * grad_ln_pi_steer
+
+            choice_acc = self.w_acc @ self.get_feature(s[t])
+            choice_acc -= np.max(choice_acc)
+            self.pi_acc = np.exp(choice_acc) / np.sum(np.exp(choice_acc))
+            for action_acc in range(5):
+                if action_acc == a[t][1]:
+                    grad_ln_pi_acc[action_acc] = (1 - self.pi_acc[action_acc]) * self.get_feature(s[t])
+                else:
+                    grad_ln_pi_acc[action_acc] = -self.pi_acc[action_acc] * self.get_feature(s[t])
+
+            w_acc_new += alpha * G * grad_ln_pi_acc
+
+            G -= r[t]
+
+        self.w_steer = w_steer_new
+        self.w_acc = w_acc_new
+
+    def linear_sarsa_lambda(self, s, a, s_dash, a_dash, r, terminate, alpha=0.125, gamma = 1.0, Lambda = 0.0):
+        s = self.get_high_level_features(s)
+        s_dash = self.get_high_level_features(s_dash)
+
+        if terminate:
+            delta_steer = r - self.w_steer[a[0]] @ s
+            delta_acc = r - self.w_acc[a[1]] @ s
+        else:
+            delta_steer = r + gamma * self.w_steer[a_dash[0]] @ s_dash - self.w_steer[a[0]] @ s
+            delta_acc = r + gamma * self.w_acc[a_dash[1]] @ s_dash - self.w_acc[a[1]] @ s
+
         self.z_steer = gamma * Lambda * self.z_steer + s
         self.w_steer[a[0]] += alpha * delta_steer * self.z_steer
 
-        delta_acc = r + gamma * self.w_acc[a_dash[1]] @ s_dash - self.w_acc[a[1]] @ s
         self.z_acc = gamma * Lambda * self.z_acc + s
         self.w_acc[a[1]] += alpha * delta_acc * self.z_acc
 
+    # Linear SARSA(λ) action logic
     def next_action(self, state):
         """
         Input: The current state
@@ -109,23 +174,46 @@ class Task1():
         """
 
         # Replace with your implementation to determine actions to be taken
-        epsilon = 1e-1
+        epsilon = 0.0
         if np.random.uniform(low=0.0, high=1.0) < epsilon:
             action_steer = np.random.randint(0, 3)
         else:
-            q_steer = self.w_steer @ self.get_feature(state)
+            q_steer = self.w_steer @ self.get_high_level_features(state)
             # action_steer = np.random.choice(np.flatnonzero(q_steer == np.max(q_steer)))
             action_steer = np.argmax(q_steer)
 
         if np.random.uniform(low=0.0, high=1.0) < epsilon:
             action_acc = np.random.randint(0, 5)
         else:
-            q_acc = self.w_acc @ self.get_feature(state)
+            q_acc = self.w_acc @ self.get_high_level_features(state)
             # action_acc = np.random.choice(np.flatnonzero(q_acc == np.max(q_acc)))
             action_acc = np.argmax(q_acc)
 
         action = np.array([action_steer, action_acc])  
         return action
+
+    # REINFORCE action logic
+    # def next_action(self, state):
+    #     """
+    #     Input: The current state
+    #     Output: Action to be taken
+    #     TO BE FILLED
+    #     """
+
+    #     # Replace with your implementation to determine actions to be taken
+    #     choice_steer = self.w_steer @ self.get_feature(state)
+    #     choice_steer -= np.max(choice_steer)
+    #     self.pi_steer = np.exp(choice_steer) / np.sum(np.exp(choice_steer))
+
+    #     choice_acc = self.w_acc @ self.get_feature(state)
+    #     choice_acc -= np.max(choice_acc)
+    #     self.pi_acc = np.exp(choice_acc) / np.sum(np.exp(choice_acc))
+        
+    #     action_steer = np.random.choice(a=np.arange(3), p=self.pi_steer)
+    #     action_acc = np.random.choice(a=np.arange(5), p=self.pi_acc)
+
+    #     action = np.array([action_steer, action_acc])  
+    #     return action
 
     def controller_task1(self, config_filepath=None, render_mode=False):
         """
@@ -164,8 +252,13 @@ class Task1():
             self.z_steer = np.zeros(self.d)
             self.z_acc = np.zeros(self.d)
 
-            a = self.next_action(self.get_feature(s))
-            # a = self.next_action(s)
+            # REINFORCE 
+            history = {}
+            history['s'] = []
+            history['a'] = []
+            history['r'] = []            
+
+            a = self.next_action(s)
             # The following code is a basic example of the usage of the simulator
             for t in range(TIMESTEPS):        
                 # Checks for quit
@@ -175,21 +268,27 @@ class Task1():
                             pygame.quit()
                             sys.exit()
 
+                # a = self.next_action(s)
                 s_dash, r, terminate, reached_road, info_dict = simulator._step(a)
                 fpsClock.tick(FPS)
 
                 cur_time += 1
 
                 a_dash = self.next_action(s_dash)
-                self.linear_sarsa_lambda(self.get_feature(s), a, self.get_feature(s_dash), a_dash, r)
-                # self.linear_sarsa_lambda(s, a, s_dash, a_dash, r)
+                self.linear_sarsa_lambda(s, a, s_dash, a_dash, r, terminate)
 
                 s = s_dash  
                 a = a_dash
 
+                history['s'].append(s)
+                history['a'].append(a)
+                history['r'].append(r)
+
                 if terminate:
                     road_status = reached_road
                     break
+
+            # self.reinforce(history)
 
             # Writing the output at each episode to STDOUT
             print(str(road_status) + ' ' + str(cur_time))
@@ -213,8 +312,8 @@ class Task2():
         """
 
         # Replace with your implementation to determine actions to be taken
-        action_steer = None
-        action_acc = None
+        action_steer = 0
+        action_acc = 0
 
         action = np.array([action_steer, action_acc])  
 
